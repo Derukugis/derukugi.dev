fb3a468a1a1356f7ee075bfd9f02b0a4f015413f
derukugi <derukugi@tuta.io>
2025-11-10 14:05:36 +1100

    grid

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    // define routes
-    let app = Router::new()
-        .nest_service("/", ServeDir::new("static"));
+    let static_files = ServeDir::new("static");
 
-    // bind & serve
+    let app = Router::new()
+        .fallback_service(static_files);
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
         .unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }
